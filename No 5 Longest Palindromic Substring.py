83524ee107804c4651ba3b45338fc852f15e7fbd
yyq <yyq@me.com>
2015-03-31 10:27:33 +0800

    No 5 AC

--- No 5 Longest Palindromic Substring.py
+++ No 5 Longest Palindromic Substring.py
@@ -4,3 +4,51 @@
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
+        lenofstr = len(s)
+        newstr = []
+        newstr.append('*')
+        i = 0
+        for x in range(lenofstr):
+            if x != lenofstr-1:
+                newstr.append(s[x])
+                newstr.append('#')
+            else:
+                newstr.append(s[x])
+                newstr.append('#')
+
+        rightbound = 0  # record the right position
+        pi = 1          # record the center
+        i = 1           # start from the 1 in newstr, since the 0 in newstr is char *
+        intarray = []   # the assist array to record all palindrome
+        intarray.insert(0, 0)
+        for x in range(1, len(newstr), 1):
+            if rightbound > x:
+                intarray.insert(x, min(rightbound-x, intarray[2*pi - x]))
+            else:
+                intarray.insert(x, 1)
+            while x-intarray[x] > 0 and x+intarray[x] < len(newstr) and newstr[x - intarray[x]] == newstr[x + intarray[x]] :
+                intarray[x] += 1
+
+            if (x+intarray[x]) > rightbound:
+                pi = x
+                rightbound = x+intarray[x]
+
+        length = max(intarray)
+        position = intarray.index(length)
+
+        resultstr = newstr[position-length+1: position+length]
+
+        print newstr
+        print intarray
+        print resultstr
+
+        resultstr = list(filter(lambda x: x != '*' and x != '#', resultstr))
+
+        return ''.join(resultstr)
+
+
+test = Solution()
+print test.longestPalindrome("abcbe")
+print test.longestPalindrome("abb")
+print test.longestPalindrome("aab")
+print test.longestPalindrome("aaa")
