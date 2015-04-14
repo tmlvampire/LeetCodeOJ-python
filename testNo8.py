733ac13416b7e7017515560c390d876f7e84e3f2
yyq <yyq@me.com>
2015-04-14 10:29:55 +0800

    UT for no8 done

--- testNo8.py
+++ testNo8.py
@@ -4,17 +4,14 @@ from No8StringtoIntegeratoi import Solution
 
 class SolutionTestCase(unittest.TestCase):
 
-
-
     def test_solution(self):
+        errorMessage = "Not equal"
         x = Solution()
-        self.assertEqual( 0 , x.atoi("-12345"), 'Not equallll')
-        # assert 1234==test.atoi("+1,234")
-        # assert 0==test.atoi("")
-        # assert 0==test.atoi("   ")
-        # assert -8, test.atoi("   -0000008")
-        # assert 0, test.atoi("        000000")
-
+        self.assertEqual(-12345, x.atoi("-12345"), errorMessage)
+        self.assertEqual(1234, x.atoi("+1,234"), errorMessage)
+        self.assertEqual(0, x.atoi("   "), errorMessage)
+        self.assertEqual(-8, x.atoi("       -008"))
+        self.assertEqual(0, x.atoi("      000000"))
 
 if __name__ == '__main__':
     unittest.main
