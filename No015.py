537fac228ee761ae8cf798e173434dbd8d412e9a
Young <yyq@me.com>
2015-05-26 00:32:06 +0800

    No 15 AC, time 50% decreased. seems ok now.

--- No015.py
+++ No015.py
@@ -22,20 +22,33 @@ class Solution15:
         for i in range(len(nums)-2):
             small = i + 1
             big = len(nums) - 1
+            if nums[i] > 0 or nums[big] < 0:
+                return result
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             while small < big:
                 sumx = nums[i] + nums[big] + nums[small]
                 if sumx == 0:
                     if [nums[i], nums[small], nums[big]] not in result:
                         result.append([nums[i], nums[small], nums[big]])
+                    temp = nums[small]
                     small += 1
-                    big -= 1
+                    while small < big and nums[small] == temp:
+                        small += 1
                 elif sumx > 0:
+                    temp = nums[big]
                     big -= 1
+                    while big > small and nums[big] == temp:
+                        big -= 1
                 elif sumx < 0:
+                    temp = nums[small]
                     small += 1
+                    while small < big and nums[small] == temp:
+                        small += 1
         return result
 
 x = Solution15()
+print x.threeSum([-1,0,1,2,-1,-4])
 print x.threeSum([-2,0,1,1,2])
 print x.threeSum([0,0,0,0])
 
