bfaa54e5bc8b91e0309b30527bc48fef8f57ade9
Young <yyq@me.com>
2015-08-31 23:23:38 +0800

    No 32 AC

---
+++ No032.py
@@ -0,0 +1,48 @@
+__author__ = 'Young'
+
+"""
+Longest Valid Parentheses Total Accepted: 43283 Total Submissions: 206335 My Submissions Question Solution
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+"""
+
+class Solution032(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        bools = [False for char in s]
+
+        stack=[]
+        for i in range(len(s)):
+            if s[i] == '(':
+                bools[i] = False
+                stack.append(i)
+            else:
+                if len(stack) != 0 and s[stack[len(stack)-1]] == '(':
+                    bools[i] = True
+                    bools[stack.pop()] = True
+                else:
+                    bools[i] = False
+                    stack.append(i)
+        max = 0
+        i = 0
+        while i < len(bools):
+            if bools[i] == True:
+                start = i
+                while i < len(bools) and bools[i] == True:
+                    i += 1
+                end = i
+                max = end - start if (end-start) > max else max
+            else:
+                i += 1
+
+        return max
+
+
+x = Solution032()
+print x.longestValidParentheses("((((()())))")
--- test_main31.py
+++ test_main31.py
@@ -1,7 +1,7 @@
 __author__ = 'Young'
-import math
 
 from No031 import Solution031
+from No032 import Solution032
 
 def test_31():
     a = Solution031()
@@ -18,3 +18,10 @@ def test_31():
     assert [3,2,1] == nums
     a.nextPermutation(nums)
     assert [1,2,3] == nums
+
+def test_32():
+    x = Solution032()
+    assert 10 == x.longestValidParentheses("((((()())))")
+    assert 2 == x.longestValidParentheses("()")
+    assert 4 == x.longestValidParentheses(")()()")
+    assert 2 == x.longestValidParentheses("))))))))()")
